31af60e643b821f09e9de54af82549f6302c4453
Max Reble <maxreble@googlemail.com>
2018-10-21 18:54:49 +0200

    Add project files.

---
+++ CorporaTextAnalyzer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CorporaTextAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
